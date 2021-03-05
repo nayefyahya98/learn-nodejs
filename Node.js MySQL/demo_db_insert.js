c21e8f0f23e18adfd704f1863d5b7b0f4055b47a
nayefyahya98 <75945162+nayefyahya98@users.noreply.github.com>
2021-03-06 01:42:14 +0200

    Update demo_db_insert.js

--- Node.js MySQL/demo_db_insert.js
+++ Node.js MySQL/demo_db_insert.js
@@ -10,7 +10,7 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "INSERT INTO customers (PersonID, LastName) VALUES ('1', 'Yahya')";
+  var sql = "INSERT INTO Persons (PersonID, LastName) VALUES ('1', 'Yahya')";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
